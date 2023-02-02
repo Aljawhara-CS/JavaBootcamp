8c1aa2b3e91f38f9082421bb2ea9161a71e1f53a
Aljawhara Alsultan <aljawhara.cs@gmail.com>
2023-02-02 16:02:42 +0300

    Update Insurance.java

--- Central_Pharmacy/src/main/java/com/example/central_pharmacy/Model/Insurance.java
+++ Central_Pharmacy/src/main/java/com/example/central_pharmacy/Model/Insurance.java
@@ -1,6 +1,8 @@
 package com.example.central_pharmacy.Model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,9 +16,15 @@ import lombok.NoArgsConstructor;
 public class Insurance {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer insuranceId;
+    @Column(columnDefinition = "varchar(20) not null")
     private String name;
+
+    @Column(columnDefinition = "varchar(20) not null")
     private String  type;
+
+    @Column(columnDefinition = "int not null")
     private Integer patientId;//foreign key
 
 
