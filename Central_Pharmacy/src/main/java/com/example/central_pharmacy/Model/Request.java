1cd86059faf776e14c72837a777b2dc92f5ac170
Aljawhara Alsultan <aljawhara.cs@gmail.com>
2023-02-02 16:29:17 +0300

    Update Request.java

--- Central_Pharmacy/src/main/java/com/example/central_pharmacy/Model/Request.java
+++ Central_Pharmacy/src/main/java/com/example/central_pharmacy/Model/Request.java
@@ -1,7 +1,8 @@
 package com.example.central_pharmacy.Model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,14 +15,25 @@ import lombok.NoArgsConstructor;
 public class Request {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id; //primary key
+    @Column(columnDefinition = "int not null")
     private Integer hospitalId;//foreign key
+    @Column(columnDefinition = "int not null")
     private Integer patientId;//foreign key
-    private Integer medicineId;//foreign key
+    @Column(columnDefinition = "int not null")
 
+    private Integer medicineId;//foreign key
+    @Column(columnDefinition = "int not null")
+    @Min(1)
     private Integer quantity; // min=1
-    private  String emergencyLevel;// rich, normal
+    @Column(columnDefinition = "varchar(20) not null")
+    @Pattern(regexp="(rush|normal)")
+    private  String emergencyLevel;// rush, normal
+    @Column(columnDefinition = "double not null")
     private double totalPrice;//  will be null then will reflect from medicine price
+    @Column(columnDefinition = "varchar(20) not null")
+    @Pattern(regexp="(waiting|rejected|approved|scheduled|delivered)")
     private String status;// waiting ,rejected,approved,scheduled,delivered
 
 
