73aff8dcd14db6807753f93f9d324a4ab666db09
Aljawhara Alsultan <aljawhara.cs@gmail.com>
2023-02-02 16:03:19 +0300

    Update Patient.java

--- Central_Pharmacy/src/main/java/com/example/central_pharmacy/Model/Patient.java
+++ Central_Pharmacy/src/main/java/com/example/central_pharmacy/Model/Patient.java
@@ -1,6 +1,8 @@
 package com.example.central_pharmacy.Model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
@@ -14,17 +16,33 @@ import lombok.NoArgsConstructor;
 public class Patient {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer patientId;//primary key
+    @Column(columnDefinition = "varchar(20) not null")
     private String name;
 
+    @Column(columnDefinition = "varchar(20) not null")
     private String age;
+    @Column(columnDefinition = "int not null")
+
     private Integer hospitalId; //foreign key
+    @Column(columnDefinition = "varchar(20) not null")
     private  String city;
+    @Column(columnDefinition = "varchar(4) not null")
+
     private  String nationalAddress;// 4 number
+    @Column(columnDefinition = "varchar(10) not null")
+
     private  String phone;
+    @Column(columnDefinition = "varchar(20) not null")
     private String email;
+
+    @Column(columnDefinition = "int not null")
     private Integer insuranceId;//foreign key
+
+    @Column(columnDefinition = "double not null")
     private double balance;
+
 
     private String StartDate;
     private String lastDate;
